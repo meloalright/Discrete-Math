264380b878e62521f015206366de2b4f514a9824
唐伟豪 <redorgreen@sina.cn>
2016-02-12 22:59:55 +0800

    cpp

--- huawei.cpp
+++ huawei.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 using namespace std;
-double qr(int n,double base){
-	if(n>1){
-		base = base /3.0;
-		return qr(n-1,base);
-	}
-	else
-		return base;
+int qr(int base){
+    if(base < 3)
+        return 0;
+    else
+    {
+        return ((base/3)+qr(base%3+base/3));
+    }
 }
 int main(){
-    double n=1.0/3;
-    double count = 0;
-    for(int i=1;i<100;i++){
-    	count+=qr(i,n);
-    	cout << "THAT "<< i <<" ="<< count << endl;
-    }
-    cout<<"FINAL = 1/3 + 1/9 + 1/27 ... + 1/n = "<< count << endl;
-    return 0;
+    int n;
+    cout << "cin n =";
+    cin >> n;
+    cout << qr(n) << endl;
 }
